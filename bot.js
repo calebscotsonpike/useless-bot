cea9bb0858718986fe6ccee4ef1ee1fdd7e35054
Caleb Pike <calebpike91@gmail.com>
2020-12-14 21:54:24 +0000

    added form submit on keyboard input Enter

--- bot.js
+++ bot.js
@@ -24,3 +24,11 @@ function handleSayClick() {
 }
 
 document.getElementById("sayit").onclick = handleSayClick;
+
+var enterClick = document.getElementById("message");
+enterClick.addEventListener("keyup", function (event) {
+  if (event.code === 'Enter') {
+    event.preventDefault();
+    document.getElementById("sayit").click();
+  }
+});
