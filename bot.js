1c3811178a50dc34e02be8b4b60691d04595dd67
Caleb Pike <calebpike91@gmail.com>
2020-12-16 16:28:49 +0000

    added reply for empty message and scrolling chat window

--- bot.js
+++ bot.js
@@ -4,11 +4,14 @@
 
 function answerQuery(query) {
   query = query.toLowerCase();
-  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
+  if (query == "")
+    return "You didn't say anything.";
+  else if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+  else if (query.indexOf("tea") != -1 || query.indexOf("coffee") != -1 || query.indexOf("drink") != -1)
+    return "I would love something to drink, but they have not created one for silicon-based life forms yet.";
   return "Sorry Dave, I'm afraid cannot do that.";
 }
-
 
 function handleSayClick() {
   // We first get the 'message' that the user entered and 'conversation' element
@@ -21,7 +24,13 @@ function handleSayClick() {
   var answer = answerQuery(message)
   conversation.innerHTML += "<p><strong>You:</strong> " + message2 + "</p>";
   conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
+  scrollToBottom();
   document.getElementById("message").value = '';
+}
+
+function scrollToBottom() {
+  var chatWindow = document.getElementById('chat-window');
+  chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
 document.getElementById("sayit").onclick = handleSayClick;
