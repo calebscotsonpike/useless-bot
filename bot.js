04e743ef7e8428bfe5a0624495748d7ebf925971
Caleb Pike <calebpike91@gmail.com>
2020-12-17 17:08:27 +0000

    changed code layout

--- bot.js
+++ bot.js
@@ -2,14 +2,15 @@
 //@edited by Louis Carter & Caleb Scotson Pike
 //@last modified 2020.12.16
 
+scrollToBottom();
+
 function answerQuery(query) {
   query = query.toLowerCase();
-  if (query == "")
-    return "You didn't say anything.";
-  else if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
+  if (query == '') return "You didn't say anything."
+  else if (query.indexOf('rain') != -1 || query.indexOf('sun') != -1 || query.indexOf('weather') != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
-  else if (query.indexOf("tea") != -1 || query.indexOf("coffee") != -1 || query.indexOf("drink") != -1)
-    return "I would love something to drink, but they have not created one for silicon-based life forms yet.";
+  else if (query.indexOf('tea') != -1 || query.indexOf('coffee') != -1 || query.indexOf('drink') != -1)
+    return 'I would love something to drink, but they have not created one for silicon-based life forms yet.';
   return "Sorry Dave, I'm afraid cannot do that.";
 }
 
@@ -21,11 +22,11 @@ function handleSayClick() {
   var conversation = document.getElementById('conversation');
 
   // Get answer for the given query and show the query & answer
-  var answer = answerQuery(message)
-  conversation.innerHTML += "<p><strong>You:</strong> " + message2 + "</p>";
-  conversation.innerHTML += "<p><strong>Useless bot:</strong> " + answer + "</p>";
+  var answer = answerQuery(message);
+  conversation.innerHTML += '<p><strong>You:</strong> ' + message2 + '</p>';
+  conversation.innerHTML += '<p><strong>Useless bot:</strong> ' + answer + '</p>';
   scrollToBottom();
-  document.getElementById("message").value = '';
+  document.getElementById('message').value = '';
 }
 
 function scrollToBottom() {
@@ -33,12 +34,12 @@ function scrollToBottom() {
   chatWindow.scrollTop = chatWindow.scrollHeight;
 }
 
-document.getElementById("sayit").onclick = handleSayClick;
+document.getElementById('sayit').onclick = handleSayClick;
 
-var enterClick = document.getElementById("message");
-enterClick.addEventListener("keyup", function (event) {
+var enterClick = document.getElementById('message');
+enterClick.addEventListener('keyup', function (event) {
   if (event.code === 'Enter') {
     event.preventDefault();
-    document.getElementById("sayit").click();
+    document.getElementById('sayit').click();
   }
 });
